87973c0aeb1233c4e707b13b575e6d1f1ca32dc4
Viktor Likhanov <volixanov@unitbean.io>
2021-05-05 23:42:08 +0300

    refactor: updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")
-        classpath(kotlin("gradle-plugin", version = "1.4.31"))
+        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath(kotlin("gradle-plugin", version = "1.5.0"))
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     }
 }
